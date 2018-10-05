ae26a3bb3d5ffb40545628a6c429fdbb4fd0e347
doctorlam <chris.lam.unt@gmail.com>
2018-10-05 16:01:35 -0500

    reorded javascript file loading

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require_tree .
 //= require activestorage
 //= require turbolinks
 //= require cocoon
-//= require_tree .
